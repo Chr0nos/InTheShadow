791a6ef68f80852d37c2be56922323fd7ff31367
Sebastien NICOLET <snicolet@student.42.fr>
2018-04-10 19:01:08 +0200

    added things

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public Vector3		finishRotation;
+	public float		tolerance = 3;
+	public Vector3		speed = new Vector3(2, 2, 2);
+	private bool		finished = false;
+
+	void Update () {
+		if (finished)
+			return ;
+		Vector3 motion = new Vector3(0, Input.GetAxis("Mouse X"), 0);
+		motion.Normalize();
+		motion.x *= speed.x;
+		motion.y *= speed.y;
+		motion.z *= speed.z;
+		transform.Rotate(motion);
+		if (isFinished())
+			finished = true;
+	}
+
+	bool isFinished()
+	{
+		Vector3		rot = transform.rotation.eulerAngles;
+
+		if (!inRange(rot.x, finishRotation.x))
+			return (false);
+		if (!inRange(rot.y, finishRotation.y))
+			return (false);
+		if (!inRange(rot.z, finishRotation.z))
+			return (false);
+		Debug.Log("finish reached");
+		return (true);
+	}
+
+	bool inRange(float x, float finish)
+	{
+		if (x > (finish + tolerance))
+			return (false);
+		if (x < (finish - tolerance))
+			return (false);
+		return (true);
+	}
+}
