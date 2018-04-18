aad8d6e4badb4878e074f664beaa8c09f7d33e7a
Sebastien NICOLET <snicolet@student.42.fr>
2018-04-18 18:27:39 +0200

    menu improvements

--- Assets/Scripts/FpsPlayer.cs
+++ Assets/Scripts/FpsPlayer.cs
@@ -1,20 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FpsPlayer : MonoBehaviour {
+	public float					maxDist = 7;
 	private CharacterController		me;
+	private MenuItem[]				items;
+	private MenuItem				currentItem = null;
 
-	// Use this for initialization
 	void Start () {
-		me = GetComponent<CharacterController>();		
+		
+		me = GetComponent<CharacterController>();
+		Cursor.lockState = CursorLockMode.Locked;
+		items = FindObjectsOfType(typeof(MenuItem)) as MenuItem[];
 	}
 	
-	// Update is called once per frame
 	void Update () {
+		MenuItem		item = RayCheck();
+
+		HighlightItem(item);
+		if (Input.GetKeyDown(KeyCode.E))
+			RayClick(item);
+		if ((Input.GetKeyDown(KeyCode.Q)) && (item))
+			item.ToggleHightlight();
 		Move();
 		transform.Rotate(-Input.GetAxis("Mouse Y"), 0, 0);
 		transform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Mouse X"));
+		if ((!currentItem) && (item))
+			item.ToggleHightlight();
+		currentItem = item;
 	}
 
 	void Move()
@@ -25,5 +40,40 @@ public class FpsPlayer : MonoBehaviour {
 		motion = transform.rotation * motion;
 		motion.y = 0;
 		me.Move(motion * 0.3f);
+	}
+
+	// launch a ray from the center of the screen forward the player
+	// can return a MenuItem or null if something else / nothing was hit
+	MenuItem	RayCheck()
+	{
+		RaycastHit		hit;
+		MenuItem		item;
+		Ray				ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+		if (!Physics.Raycast(ray, out hit))
+			return (null);
+		if (hit.distance > maxDist)
+			return (null);
+		item = hit.collider.gameObject.GetComponent<MenuItem>();
+		if (item)
+			Debug.DrawLine(Camera.main.transform.position, hit.point, Color.cyan);
+		return (item);
+	}
+
+	void RayClick(MenuItem item)
+	{
+		if (item)
+			item.OnClick();
+	}
+
+	void HighlightItem(MenuItem item)
+	{
+		Color		color;
+
+		foreach (MenuItem citem in items)
+		{
+			color = (citem == item) ? Color.green : Color.white;
+			citem.GetComponent<MeshRenderer>().material.color = color;
+		}
 	}
 }
---
+++ Assets/Scripts/MenuItem.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuItem : MonoBehaviour {
+	public string	levelName;
+	public string	levelMap;
+	public bool		forceAvailable; 
+	public bool		quit = false;
+	private bool	ready;
+	private bool	inCoroutine = false;
+
+	private void Start()
+	{
+		if (forceAvailable)
+			ready = true;
+		else
+			ready = IsAvailable();
+	}
+
+	private void Update()
+	{
+		if (ready)
+			transform.Rotate(Vector3.up, Space.World);
+	}
+
+	public bool IsAvailable()
+	{
+		if (forceAvailable)
+			return (true);
+		return (PlayerPrefs.GetInt(levelMap, 0) == 1);
+	}
+
+	public void OnClick()
+	{
+		Debug.Log("Click trigger " + name);
+		if ((!forceAvailable) && (!IsAvailable()))
+			return ;
+		if (quit)
+			Application.Quit();
+		else
+		{
+			Debug.Log("Loading " + levelMap);
+			SceneManager.LoadScene(levelMap);
+		}
+	}
+
+	// just wanted to try coroutines
+	// this thing take the original scale then max it by 50% then lower it by 50%
+	// and returns to the original size.
+	IEnumerator Highlight()
+	{
+		float			original = transform.localScale.x;
+		float			max = original * 1.5f;
+		float			min = original * 0.5f;
+		float			delta = 0.025f;
+
+		inCoroutine = true;
+		for (float x = transform.localScale.x; x < max; x += delta)
+		{
+			transform.localScale = new Vector3(x, x, x);
+			yield return null;
+		}
+		for (float x = max; x > min; x -= delta)
+		{
+			transform.localScale = new Vector3(x, x, x);
+			yield return null;
+		}
+		for (float x = min; x < original; x += delta)
+		{
+			transform.localScale = new Vector3(x, x, x);
+			yield return null;
+		}
+		inCoroutine = false;
+	}
+
+	public void ToggleHightlight()
+	{
+		if (!inCoroutine)
+			StartCoroutine("Highlight");
+	}
+}
